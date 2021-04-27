fd50ba974249412a7362c28b0fe68996957bf23c
Rohan Cp <rohancp9@icloud.com>
2021-04-28 00:46:14 +0530

    fav icon added

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,40 +1,52 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
 import { MEALS } from '../data/dummy-data';
-import Colors from '../constants/Colors';
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
 
-  const displayMeal = MEALS.find(meal => meal.id === mealId);
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
   return (
     <View style={styles.screen}>
-      <Text>{displayMeal.title}</Text>
-      <Text>{displayMeal.steps}</Text>
-      <Text>{displayMeal.complexity}</Text>
-      <Text>{displayMeal.affordability}</Text>
+      <Text>{selectedMeal.title}</Text>
+      <Button
+        title="Go Back to Categories"
+        onPress={() => {
+          props.navigation.popToTop();
+        }}
+      />
     </View>
   );
 };
 
-MealDetailScreen.navigationOptions = (navigationData) => {
+MealDetailScreen.navigationOptions = navigationData => {
   const mealId = navigationData.navigation.getParam('mealId');
-  const displayMeal = MEALS.find(meal => meal.id === mealId);
-
-  return ({
-    headerTitle: displayMeal.title,
-    headerStyle: {
-      backgroundColor: Colors.primaryColor,
-    },
-    headerTintColor: 'white',
-  });
-}
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  return {
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log('Mark as favorite!');
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: 'violet'
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
